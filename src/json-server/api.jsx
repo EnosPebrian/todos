7d2773db6593b9d843dc91b32dd200063cb05272
enospebrian1 <54843640+enospebrian1@users.noreply.github.com>
2023-08-15 06:32:14 +0700

    Update api.jsx

--- src/json-server/api.jsx
+++ src/json-server/api.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:2000/" });
+const api = axios.create({ baseURL: "https://spectrum-handsome-station.glitch.me/" });
 
 export default api;
